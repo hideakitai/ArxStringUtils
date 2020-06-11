b70dab75cd7dd761ab500511989b9cdef9d17602
Hideaki Tai <hideaki.tai@gmail.com>
2020-06-11 18:10:26 +0900

    update to v0.2.0

---
+++ ArxStringUtil.h
@@ -0,0 +1,239 @@
+#pragma once
+#ifndef ARX_STRINGUTIL_H
+#define ARX_STRINGUTIL_H
+
+#include <Arduino.h>
+#include "util/ArxTypeTraits/ArxTypeTraits.h"
+#include <type_traits>
+
+namespace arx {
+namespace str {
+
+    namespace detail
+    {
+        template <size_t size>
+        struct same_size_int;
+
+        template <size_t size>
+        using same_size_int_t = typename same_size_int<size>::type;
+
+        template <> struct same_size_int<1> { using type = int8_t; };
+        template <> struct same_size_int<2> { using type = int16_t; };
+        template <> struct same_size_int<4> { using type = int32_t; };
+        template <> struct same_size_int<8> { using type = int64_t; };
+
+        template <typename T, typename std::enable_if<std::is_floating_point<T>::value>::type* = nullptr>
+        struct IntFloatUnion_impl {
+            using type = union {
+                same_size_int_t<sizeof(T)> x;
+                T f;
+            };
+        };
+
+        template <typename T>
+        using IntFloatUnion = typename IntFloatUnion_impl<T>::type;
+
+        template <typename T>
+        inline auto to_int(const String &intString)
+        -> typename std::enable_if<std::is_integral<T>::value, T>::type
+        {
+            return (T)intString.toInt();
+        }
+    }
+
+    template <typename T, size_t length = sizeof(T) * 2>
+    inline auto to_hex(const T& value, bool b_leading_zeros = true)
+    -> typename std::enable_if<std::is_integral<T>::value, String>::type
+    {
+        String format;
+        if (b_leading_zeros) format = "%0" + String(length) + "X";
+        else                 format = "%X";
+        char hex[length + 1];
+        sprintf(hex, format.c_str(), value);
+        return String(hex);
+    }
+
+    template <typename T, size_t length = sizeof(T) * 2>
+    inline auto to_hex(const T& value, bool b_leading_zeros = true)
+    -> typename std::enable_if<std::is_floating_point<T>::value, String>::type
+    {
+        detail::IntFloatUnion<T> myUnion;
+        myUnion.f = value;
+        return to_hex(myUnion.x, b_leading_zeros);
+    }
+
+    inline int from_hex_to_int(const String& intHexString)
+    {
+        return (int)strtol(intHexString.c_str(), NULL, 16);
+    }
+
+    inline char from_hex_to_char(const String& charHexString)
+    {
+        return (char)strtol(charHexString.c_str(), NULL, 16);
+    }
+
+    inline float from_hex_to_float(const String& floatHexString)
+    {
+        detail::IntFloatUnion<float> myUnion;
+        myUnion.x = from_hex_to_int(floatHexString);
+        return myUnion.f;
+    }
+
+    inline double from_hex_to_double(const String& doubleHexString)
+    {
+        detail::IntFloatUnion<double> myUnion;
+        myUnion.x = from_hex_to_int(doubleHexString);
+        return myUnion.f;
+    }
+
+    template <typename T>
+    inline auto from_dec_to_bcd(const T& n)
+    -> typename std::enable_if<std::is_integral<T>::value, size_t>::type
+    {
+        return n + 6 * (n / 10);
+    }
+
+    template <typename T>
+    inline auto from_bcd_to_dec(const T& n)
+    -> typename std::enable_if<std::is_integral<T>::value, size_t>::type
+    {
+        return n - 6 * (n >> 4);
+    }
+
+    template <typename T>
+    inline auto string_length(const T& value)
+    -> typename std::enable_if<std::is_integral<T>::value, size_t>::type
+    {
+        return floor(log10(value) + 1);
+    }
+
+    template <typename T>
+    inline auto to_string(const T& value, size_t width)
+    -> typename std::enable_if<std::is_integral<T>::value, String>::type
+    {
+        String format;
+        format = "%0" + String(width) + "d";
+        const size_t str_len = string_length(value);
+        const size_t len = (width > str_len) ? width : str_len;
+        char dec[len + 1];
+        sprintf(dec, format.c_str(), value);
+        return String(dec);
+    }
+
+    template <typename T>
+    inline auto to_string(const T& value)
+    -> typename std::enable_if<std::is_integral<T>::value, String>::type
+    {
+        return to_string(value, 0);
+    }
+
+    template <typename T>
+    inline auto to_string(const T& value, size_t precision, size_t width)
+    -> typename std::enable_if<std::is_floating_point<T>::value, String>::type
+    {
+        String format;
+        format = "%0" + String(width) + "." + String(precision) + "f";
+        const size_t str_len = String(value).length();
+        const size_t len = (width > str_len) ? width : str_len;
+        char dec[len + 1];
+        sprintf(dec, format.c_str(), value);
+        return String(dec);
+    }
+
+    template <typename T>
+    inline auto to_string(const T& value, size_t precision)
+    -> typename std::enable_if<std::is_floating_point<T>::value, String>::type
+    {
+        return to_string(value, precision, 0);
+    }
+
+
+#ifndef __AVR__
+
+    #include <vector>
+
+    inline std::vector<String> split_string(const String& s, const String& delim)
+    {
+        std::vector<String> result;
+        std::vector<size_t> pos;
+        // std::vector<size_t> length;
+        pos.emplace_back(0);
+        for (size_t i = 0; i < s.length(); ++i)
+        {
+            if (s.charAt(i) == *(delim.c_str()))
+            {
+                pos.emplace_back(i + 1);
+            }
+            else if (s.charAt(i) == '\r')
+            {
+                pos.emplace_back(i + 1);
+                break;
+            }
+            else if (s.charAt(i) == '\n')
+            {
+                pos.emplace_back(i + 1);
+                break;
+            }
+        }
+        for (size_t i = 0; i < pos.size() - 1; ++i)
+        {
+            result.push_back(s.substring(pos[i], pos[i + 1] - 1));
+        }
+        if (!s.endsWith(String('\n')))
+        {
+            result.push_back(s.substring(pos[pos.size() - 1], s.length()));
+        }
+        return result; // local value will be moved
+    }
+
+#endif
+
+//    String to_bin(const String& value)
+//    {
+//        String out("");
+//        size_t numBytes = value.length();
+//        for(size_t i = 0; i < numBytes; i++) {
+//            std::bitset<8> bitBuffer(value.c_str()[i]);
+//            out += String(bitBuffer.to_ulong(), BIN);
+//        }
+//        return out;
+//    }
+//
+//    String to_bin(const char* value) { return toBinary(String(value)); }
+//
+//    int from_bin_to_int(const String& value)
+//    {
+//        const size_t intSize = sizeof(int) * 8;
+//        std::bitset<intSize> binaryString(value.c_str()[0]);
+//        return (int)binaryString.to_ulong();
+//    }
+//
+//    char from_bin_to_char(const String& value)
+//    {
+//        const size_t charSize = sizeof(char) * 8;
+//        std::bitset<charSize> binaryString(value.c_str()[0]);
+//        return (char) binaryString.to_ulong();
+//    }
+//
+//    float from_bin_to_float(const String& value)
+//    {
+//        const size_t floatSize = sizeof(float) * 8;
+//        std::bitset<floatSize> binaryString(value.c_str()[0]);
+//        union ulongFloatUnion {
+//                unsigned long result;
+//                float f;
+//        } myUFUnion;
+//        myUFUnion.result = binaryString.to_ulong();
+//        return myUFUnion.f;
+//    }
+//
+//    template <class T>
+//    String to_bin(const T& value) {
+//        return std::bitset<8 * sizeof(T)>(*reinterpret_cast<const uint64_t*>(&value)).to_string();
+//    }
+
+} // namespace str
+} // namespace arx
+
+
+#endif // EMBEDDEDUTILS_CALCULUS_H
--- example/example.ino
+++ example/example.ino
@@ -1,33 +1,122 @@
-#include <Convert.h>
+#include <ArxStringUtil.h>
+#include <cassert>
 
 void setup()
 {
     Serial.begin(115200);
     delay(2000);
 
-    String hex = Convert::to_hex(12345);
-    String hex_no_zero = Convert::to_hex(12345, false);
-    Serial.println(hex);
-    Serial.println(hex_no_zero);
-    Serial.println(Convert::from_hex_to_int(hex));
+    Serial.print("hex string of '0x123' is ");
+    Serial.println(arx::str::to_hex(0x123, false)); // with no leading zeros
+    assert(arx::str::to_hex((uint8_t)0x12) == "12");
+    assert(arx::str::to_hex((uint16_t)0x123) == "0123");
+    assert(arx::str::to_hex((uint16_t)0x1234) == "1234");
+    assert(arx::str::to_hex((uint32_t)0x12345) == "00012345");
+    assert(arx::str::to_hex((uint32_t)0x123456) == "00123456");
+    assert(arx::str::to_hex((uint32_t)0x1234567) == "01234567");
+    assert(arx::str::to_hex((uint32_t)0x12345678) == "12345678");
 
-    String hex_s = "49"; // 0x49
-    Serial.println((int)Convert::from_hex_to_char(hex_s)); // 73
-    Serial.println((int)Convert::from_hex_to_char(hex_s), HEX); // 0x49
+    assert(arx::str::to_hex((uint8_t)0x12, false) == "12");
+    assert(arx::str::to_hex((uint16_t)0x123, false) == "123");
+    assert(arx::str::to_hex((uint16_t)0x1234, false) == "1234");
+    assert(arx::str::to_hex((uint32_t)0x12345, false) == "12345");
+    assert(arx::str::to_hex((uint32_t)0x123456, false) == "123456");
+    assert(arx::str::to_hex((uint32_t)0x1234567, false) == "1234567");
+    assert(arx::str::to_hex((uint32_t)0x12345678, false) == "12345678");
 
-    // TODO: FIXME:
-    String hex_f = Convert::to_hex(12.345);
-    Serial.println(hex_f);
-    Serial.println(Convert::from_hex_to_float(hex_f));
-    Serial.println(Convert::from_hex_to_double(hex_f));
+    Serial.print("value of hex string '123' is ");
+    Serial.println(arx::str::from_hex_to_int("123"), HEX);
+    assert(arx::str::from_hex_to_int(arx::str::to_hex(0x12)) == 0x12);
+    assert(arx::str::from_hex_to_int(arx::str::to_hex(0x1234)) == 0x1234);
+    assert(arx::str::from_hex_to_int(arx::str::to_hex(0x123456)) == 0x123456);
+    assert(arx::str::from_hex_to_int(arx::str::to_hex(0x12345678)) == 0x12345678);
 
-    // TODO: FIXME:
-    String text = "one,two,three,four\n";
-    auto strs1 = Convert::split_string(text, ",");
-    for (auto& s : strs1) Serial.println(s);
+    Serial.print("hex string of '1.2f' is ");
+    Serial.println(arx::str::to_hex(1.2f, false)); // with no leading zeros
+    Serial.print("value of hex string '3F99999A' is ");
+    Serial.println(arx::str::from_hex_to_float("3F99999A"));
+    assert(arx::str::from_hex_to_float(arx::str::to_hex(1.2f)) == 1.2f);
+    assert(arx::str::from_hex_to_float(arx::str::to_hex(12.34f)) == 12.34f);
+    assert(arx::str::from_hex_to_float(arx::str::to_hex(123.456f)) == 123.456f);
+    assert(arx::str::from_hex_to_float(arx::str::to_hex(1234.5678f)) == 1234.5678f);
+    assert(arx::str::from_hex_to_float(arx::str::to_hex(12345.67890f)) == 12345.67890f);
 
-    // auto strs2 = Convert::split_string(text, " ");
-    // for (auto& s : strs2) Serial.println(s);
+    // FIXME:
+    // assert(arx::str::from_hex_to_double(arx::str::to_hex(1.2)) == 1.2);
+    // assert(arx::str::from_hex_to_double(arx::str::to_hex(12.34)) == 12.34);
+    // assert(arx::str::from_hex_to_double(arx::str::to_hex(123.456)) == 123.456);
+    // assert(arx::str::from_hex_to_double(arx::str::to_hex(1234.5678)) == 1234.5678);
+    // assert(arx::str::from_hex_to_double(arx::str::to_hex(12345.67890)) == 12345.67890);
+
+    Serial.print("bcd of hex '0x12' is ");
+    Serial.println(arx::str::from_dec_to_bcd(0x12), BIN);
+    Serial.print("dec of bcd '12' is ");
+    Serial.println(arx::str::from_bcd_to_dec(12), BIN);
+    assert(arx::str::from_bcd_to_dec(arx::str::from_dec_to_bcd(0x1)) == 0x1);
+    assert(arx::str::from_bcd_to_dec(arx::str::from_dec_to_bcd(0x12)) == 0x12);
+    assert(arx::str::from_bcd_to_dec(arx::str::from_dec_to_bcd(0x123)) == 0x123);
+    assert(arx::str::from_bcd_to_dec(arx::str::from_dec_to_bcd(0x1234)) == 0x1234);
+    assert(arx::str::from_bcd_to_dec(arx::str::from_dec_to_bcd(0x12345)) == 0x12345);
+    assert(arx::str::from_bcd_to_dec(arx::str::from_dec_to_bcd(0x123456)) == 0x123456);
+    assert(arx::str::from_bcd_to_dec(arx::str::from_dec_to_bcd(0x1234567)) == 0x1234567);
+    assert(arx::str::from_bcd_to_dec(arx::str::from_dec_to_bcd(0x12345678)) == 0x12345678);
+
+    Serial.print("length of '1234' is ");
+    Serial.println(arx::str::string_length(1234));
+    assert(arx::str::string_length(1) == 1);
+    assert(arx::str::string_length(12) == 2);
+    assert(arx::str::string_length(123) == 3);
+    assert(arx::str::string_length(1234) == 4);
+    assert(arx::str::string_length(12345) == 5);
+    assert(arx::str::string_length(123456) == 6);
+    assert(arx::str::string_length(1234567) == 7);
+    assert(arx::str::string_length(12345678) == 8);
+    assert(arx::str::string_length(123456789) == 9);
+
+    Serial.print("convert int '1234' to string: ");
+    Serial.println(arx::str::to_string(1234));
+    assert(arx::str::to_string(1) == "1");
+    assert(arx::str::to_string(12) == "12");
+    assert(arx::str::to_string(123) == "123");
+    assert(arx::str::to_string(1234) == "1234");
+    assert(arx::str::to_string(12345) == "12345");
+    assert(arx::str::to_string(123456) == "123456");
+    assert(arx::str::to_string(1234567) == "1234567");
+    assert(arx::str::to_string(12345678) == "12345678");
+    assert(arx::str::to_string(123456789) == "123456789");
+
+    Serial.print("convert int '1234' to string in width 8: ");
+    Serial.println(arx::str::to_string(1234, 8));
+    assert(arx::str::to_string(1, 8) == "00000001");
+    assert(arx::str::to_string(12, 8) == "00000012");
+    assert(arx::str::to_string(123, 8) == "00000123");
+    assert(arx::str::to_string(1234, 8) == "00001234");
+    assert(arx::str::to_string(12345, 8) == "00012345");
+    assert(arx::str::to_string(123456, 8) == "00123456");
+    assert(arx::str::to_string(1234567, 8) == "01234567");
+    assert(arx::str::to_string(12345678, 8) == "12345678");
+
+    Serial.print("convert '1.2' to string with precision 2: ");
+    Serial.println(arx::str::to_string(1.2, 2));
+    Serial.print("convert '1.234' to string with precision 2: ");
+    Serial.println(arx::str::to_string(1.234, 2));
+    Serial.print("convert '123.4' to string with precision 2: ");
+    Serial.println(arx::str::to_string(123.4, 2));
+
+    Serial.print("convert '123.456789' to string with precision 2 in width 4: ");
+    Serial.println(arx::str::to_string(123.456789, 2, 4));
+    Serial.print("convert '123.456789' to string with precision 2 in width 8: ");
+    Serial.println(arx::str::to_string(123.456789, 2, 8));
+
+    auto strs1 = arx::str::split_string("one,two,three,four", ",");
+    Serial.print("string 'one,two,three' is devided to : ");
+    for (auto& s : strs1) { Serial.print(s); Serial.print(", "); }
+    Serial.println();
+
+    auto strs2 = arx::str::split_string("one,two,three,four\n", ",");
+    Serial.print("string 'one,two,three' is devided to : ");
+    for (auto& s : strs2) { Serial.print(s); Serial.print(", "); }
+    Serial.println();
 }
 
 void loop()
